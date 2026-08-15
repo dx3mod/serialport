81ca4cf1e1a1954a66eede75e6ae60133d473ce1
dx3mod <kxyzd@vk.com>
2026-08-15 15:39:10 +0300

    Add Descriptor module

---
+++ lib/descriptor.ml
@@ -0,0 +1,68 @@
+type t = {
+  fd : Unix.file_descr;
+  name : string;
+  mutable configuration : Configuration.t option;
+}
+
+let to_unix_fd { fd; _ } = fd
+and of_unix_fd ?(name = "UNKNOWN") fd = { fd; configuration = None; name }
+
+let to_channels ?(buffering = false) { fd; _ } =
+  let ic = Unix.in_channel_of_descr fd and oc = Unix.out_channel_of_descr fd in
+  Out_channel.set_buffered oc buffering;
+
+  (ic, oc)
+
+(* Native bindings functions  *)
+module Intf = struct
+  [@@@warning "-37"]
+
+  external flush : Unix.file_descr -> unit = "caml_flush_serial_port"
+  external drain : Unix.file_descr -> unit = "caml_drain_serial_port"
+
+  external configure : Unix.file_descr -> Configuration.t -> unit
+    = "caml_configure_serial_port"
+
+  external get_configuration : Unix.file_descr -> Configuration.t
+    = "caml_get_configuration_serial_port"
+
+  type modem_bits = Rts | Cts | Dsr | Dcd | Dtr | Ri
+
+  external get_pin : Unix.file_descr -> modem_bits -> bool
+    = "caml_get_serial_port_pin"
+
+  external set_pin : Unix.file_descr -> modem_bits -> bool -> unit
+    = "caml_set_serial_port_pin"
+
+  external send_break : Unix.file_descr -> unit
+    = "caml_send_break_signal_to_serial_port"
+end
+
+let configure pd config =
+  pd.configuration <- Some config;
+  Intf.configure pd.fd config
+
+let configuration pd =
+  match pd.configuration with
+  | None ->
+      let configuration = Intf.get_configuration pd.fd in
+      pd.configuration <- Some configuration;
+      configuration
+  | Some configuration -> configuration
+
+module Modem = struct
+  let set_request_to_send { fd; _ } high = Intf.set_pin fd Rts high
+  and set_data_terminal_ready { fd; _ } high = Intf.set_pin fd Dtr high
+
+  let get_request_to_send { fd; _ } = Intf.get_pin fd Rts
+  and get_data_terminal_ready { fd; _ } = Intf.get_pin fd Dtr
+end
+
+let send_break_signal { fd; _ } = Intf.send_break fd
+
+let flush { fd; _ } = Intf.flush fd
+and drain { fd; _ } = Intf.drain fd
+
+let pp ppf pd =
+  Format.fprintf ppf "Serialport.Descriptor(%S, %s)" pd.name
+    Configuration.(to_string @@ configuration pd)
---
+++ lib/descriptor.mli
@@ -0,0 +1,110 @@
+(** The descriptor module of a serial port provides types and low-level
+    functions. *)
+
+type t
+(** Descriptor type. *)
+
+(** {1 Construction} *)
+
+val of_unix_fd : ?name:string -> Unix.file_descr -> t
+(** [of_unix_fd ?name fd]
+
+    Construct a serial port descriptor from a file descriptor with the passed
+    name.
+
+    {b Example}
+
+    {[
+    let pd = Serialport.Descriptor.of_unix_fd ~name:"/dev/ttys003" fd in
+    ]}
+
+    @param ?name
+      It is optional to use a pretty name for a descriptor. By default, it is
+      "UNKNOWN". *)
+
+val to_unix_fd : t -> Unix.file_descr
+(** [to_unix_fd pd]
+
+    Extract a file descriptor from the serial port descriptor. *)
+
+(** {2 Conversions} *)
+
+val to_channels : ?buffering:bool -> t -> in_channel * out_channel
+(** [to_channel ?buffering pd]
+
+    Create an input and output channels from the serial port descriptor.
+
+    @param ?buffering Set buffering for the output channel. Default is false. *)
+
+(** {1 Configuration} *)
+
+val configure : t -> Configuration.t -> unit
+(** [configure pd configuration]
+
+    Apply all [configuration] options to the serial port descriptor. See
+    {!Configuration.make} or {!Configuration.of_string} functions for creating
+    options.
+
+    {b Example}
+
+    {[
+    Serialport.Configuration.of_string ~baud_rate:9800 "8N1H"
+    |> Serialport.Descriptor.configure pd
+    ]}
+
+    @raise Failure if something went wrong *)
+
+val configuration : t -> Configuration.t
+(** [configuration pd]
+
+    Returns an actual serial port configuration. *)
+
+(** {1 Modem} *)
+
+(** A serial port modem module. *)
+module Modem : sig
+  (** {1 Setting pins} *)
+
+  val set_request_to_send : t -> bool -> unit
+  (** [set_request_to_send pd high]
+
+      Sets the state of the RTS (Request To Send) control signal. *)
+
+  val set_data_terminal_ready : t -> bool -> unit
+  (** [set_data_terminal_ready pd high]
+
+      Sets the state of the Data Terminal Ready pin. *)
+
+  (** {1 Getting pins} *)
+
+  val get_request_to_send : t -> bool
+  (** [get_request_to_send pd] *)
+
+  val get_data_terminal_ready : t -> bool
+  (** [get_data_terminal_ready pd] *)
+end
+
+(** {1 Other} *)
+
+(** {2 Flushing} *)
+
+val flush : t -> unit
+(** [flush pd]
+
+    Flush the serial port. *)
+
+val drain : t -> unit
+(** [drain pd]
+
+    Drain the serial port. *)
+
+(** {2 Break signal} *)
+
+val send_break_signal : t -> unit
+(** [send_break_signal pd]
+
+    Send a BREAK signal to the serial port. *)
+
+(** {2 Pretty print} *)
+
+val pp : Format.formatter -> t -> unit [@@ocaml.toplevel_printer]
